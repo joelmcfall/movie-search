27219bf732f732aa96cfb5dee248095c8866dbe9
Joel McFall <wjsmcfall@gmail.com>
2020-02-11 13:33:02 +0000

    Updates

---
+++ precache-manifest.9ce88cf8f4c5a94c2c529d54d8e09bca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eaed6b43c4dc1d74ed7d4205aefecd61",
+    "url": "/movie-search/index.html"
+  },
+  {
+    "revision": "cd34fadda2af57d2b0ce",
+    "url": "/movie-search/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "f1d3919e0b8794559d24",
+    "url": "/movie-search/static/css/main.bb974958.chunk.css"
+  },
+  {
+    "revision": "cd34fadda2af57d2b0ce",
+    "url": "/movie-search/static/js/2.0b22148c.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/movie-search/static/js/2.0b22148c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1d3919e0b8794559d24",
+    "url": "/movie-search/static/js/main.6c629495.chunk.js"
+  },
+  {
+    "revision": "4d4dc7cbd1edb90d907f",
+    "url": "/movie-search/static/js/runtime-main.3772cad2.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/movie-search/precache-manifest.48ba9df77e34e7727bd42f84d7e49cb4.js"
+  "/movie-search/precache-manifest.9ce88cf8f4c5a94c2c529d54d8e09bca.js"
 );
 
 self.addEventListener('message', (event) => {
