f15c01936778a3a67302a47e74538a7111bd806c
Joel McFall <wjsmcfall@gmail.com>
2020-02-22 14:42:16 +0000

    Updates

--- precache-manifest.f8c283b4e004851c1fffe43512bce711.js
+++ precache-manifest.f8c283b4e004851c1fffe43512bce711.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "fe49251207262f46f586e9c049d7e406",
+    "revision": "7ee279375da9932bb172b265b8a44308",
     "url": "/movie-search/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/movie-search/static/css/2.47e06e2e.chunk.css"
   },
   {
-    "revision": "3c5b0ce22cdcc69e31c8",
-    "url": "/movie-search/static/css/main.52b07bd8.chunk.css"
+    "revision": "a274063e0576c88d5205",
+    "url": "/movie-search/static/css/main.db9e0d72.chunk.css"
   },
   {
     "revision": "cd34fadda2af57d2b0ce",
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/movie-search/static/js/2.0b22148c.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "3c5b0ce22cdcc69e31c8",
-    "url": "/movie-search/static/js/main.d115f8e0.chunk.js"
+    "revision": "a274063e0576c88d5205",
+    "url": "/movie-search/static/js/main.dc57a3fe.chunk.js"
   },
   {
     "revision": "4d4dc7cbd1edb90d907f",
