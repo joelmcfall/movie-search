4b55e4d63a4ed1cb56752b958da17b134690ad04
Joel McFall <wjsmcfall@gmail.com>
2020-02-22 14:41:51 +0000

    Fixed layout removed junk

--- src/components/searchbar.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from "react";
-
-import { Row, Col, Button, Form, FormControl } from "react-bootstrap";
-
-class SearchBar extends Component {
-  render() {}
-}
-export default SearchBar;
